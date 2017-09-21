2c8740e327f16bfe22e7d7144cee908fdad33251
dianlujitao <dianlujitao@lineageos.org>
2017-09-21 22:17:21 +0800

    op3: Update blobs from OB24 / OB15

--- oneplus3/Android.mk
+++ oneplus3/Android.mk
@@ -265,6 +265,4 @@ ifneq ($(TARGET_HAVE_QC_PERF),true)
 
 endif
 
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 endif
