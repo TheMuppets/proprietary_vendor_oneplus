3d1c02a8e6d9fd97f85ae753d7f2716454ffbbd1
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2017-11-26 20:04:39 +0100

    oneplus2: run extract-files script
    Change-Id: Id8bf4f533bf1d9b26950a3f62fa8b01d0557e38b

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -22,7 +22,7 @@ ifeq ($(TARGET_DEVICE),oneplus2)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES_64 := proprietary/lib64/libloc_api_v02.so
+LOCAL_SRC_FILES := proprietary/lib64/libloc_api_v02.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -32,7 +32,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_ds_api
 LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES_64 := proprietary/lib64/libloc_ds_api.so
+LOCAL_SRC_FILES := proprietary/lib64/libloc_ds_api.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
