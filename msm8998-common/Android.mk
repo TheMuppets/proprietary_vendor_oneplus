3b99bd0a88c93905cfbec0f9634905f816ce2702
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-28 19:56:15 +0100

    msm8998: add additional qcom/qti services and blobs from OOS 5.0
    Change-Id: Iea25651c2a68d26e295a1daee9a06faeed70cb0f

--- msm8998-common/Android.mk
+++ msm8998-common/Android.mk
@@ -261,6 +261,17 @@ LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := vendor.qti.hardware.alarm-V1.0-java
+LOCAL_MODULE_OWNER := oneplus
+LOCAL_SRC_FILES := proprietary/vendor/framework/vendor.qti.hardware.alarm-V1.0-java.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.sensorscalibrate-V1.0-java
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/vendor/framework/vendor.qti.hardware.sensorscalibrate-V1.0-java.jar
