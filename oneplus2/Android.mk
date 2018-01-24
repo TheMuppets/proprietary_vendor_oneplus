2e659fb89c0228afcfdae43781098228bf24326e
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-01-24 11:34:10 +0100

    oneplus2: remove QtiTetherService.apk
    *also remove ununsed apk while we are at it
    
    Change-Id: I8826cebee791114d58c8d92f0f361fd7cebeb00d

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -237,18 +237,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := QtiTetherService
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/priv-app/QtiTetherService/QtiTetherService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
