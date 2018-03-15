e7b646ac69eb6b79d2e1bef83aa9af112ed9acd8
Bruno Martins <bgcngm@gmail.com>
2018-03-15 16:47:31 +0800

    op3: Use O-MR1 based display and media blobs
     * From: bq/bardock-pro/bardock-pro:8.1.0/OPM1.171019.011/1651:user/release-keys
    
    Change-Id: Ic64dd45091d547e2cd78a62ed8d126cd8bf9c5ae

--- oneplus3/Android.mk
+++ oneplus3/Android.mk
@@ -205,18 +205,6 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := colorservice
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/vendor/app/colorservice/colorservice.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcnvitems
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/framework/qcnvitems.jar
