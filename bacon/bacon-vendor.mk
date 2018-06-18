76ed698108c3f69ba26c7513ea309ce3342a372b
Michael Bestas <mkbestas@lineageos.org>
2018-06-19 00:36:15 +0300

    bacon: Remove unused camera firmware
    * msm8974 camera uses v1_2_0
    
    Change-Id: I754194dd54585682ebe031c34c27e9002bd144f2

--- bacon/bacon-vendor.mk
+++ bacon/bacon-vendor.mk
@@ -17,8 +17,6 @@
 
 PRODUCT_COPY_FILES += \
     vendor/oneplus/bacon/proprietary/vendor/bin/mm-qcamera-daemon:system/vendor/bin/mm-qcamera-daemon \
-    vendor/oneplus/bacon/proprietary/vendor/firmware/cpp_firmware_v1_1_1.fw:system/vendor/firmware/cpp_firmware_v1_1_1.fw \
-    vendor/oneplus/bacon/proprietary/vendor/firmware/cpp_firmware_v1_1_6.fw:system/vendor/firmware/cpp_firmware_v1_1_6.fw \
     vendor/oneplus/bacon/proprietary/vendor/firmware/cpp_firmware_v1_2_0.fw:system/vendor/firmware/cpp_firmware_v1_2_0.fw \
     vendor/oneplus/bacon/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     vendor/oneplus/bacon/proprietary/vendor/lib/libAKM8963.so:system/vendor/lib/libAKM8963.so \
