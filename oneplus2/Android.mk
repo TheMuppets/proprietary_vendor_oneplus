b3b7dea1dd5c2a2479169aeb68cba98ed5c6d76e
Wang Han <416810799@qq.com>
2018-07-11 17:13:17 +0800

    oneplus2: Copy jars instead of building them

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -251,26 +251,6 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := qcnvitems
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/framework/qcnvitems.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := qcrilhook
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/framework/qcrilhook.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := pp_calib_data_jd35695_1080p_cmd_mode_dsi_panel
 LOCAL_MODULE_OWNER := oneplus
 LOCAL_SRC_FILES := proprietary/etc/pp_calib_data_jd35695_1080p_cmd_mode_dsi_panel.xml
