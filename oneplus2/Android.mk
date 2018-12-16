0444f1aa63aafbf9ab2d2e364824daef5e13efaf
Wang Han <416810799@qq.com>
2018-12-16 17:49:42 +0800

    oneplus2: Remove egl symlinks generation

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -223,7 +223,4 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
 include $(BUILD_PREBUILT)
 
-
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
-$(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 endif
