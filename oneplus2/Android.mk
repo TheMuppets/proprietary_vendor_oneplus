efdfdaba20acbf9f6cef988eefb4eec15f5dd116
Wang Han <wanghan1995315@gmail.com>
2020-05-26 17:02:03 +0800

    oneplus2: Drop time service blobs except stock libtime_genoff.so
     * All of a sudden, time_genoff connection will fail and sometimes
       block unlocking with password, etc. Stock libtime_genoff.so will
       fix it in most cases, but not all. Drop time service stack to make
       this connection fail at start to workaround the problem. Time
       service is broken on 10 anyway so it is a good trade-off.

--- oneplus2/Android.mk
+++ oneplus2/Android.mk
@@ -202,16 +202,4 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := TimeService
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
