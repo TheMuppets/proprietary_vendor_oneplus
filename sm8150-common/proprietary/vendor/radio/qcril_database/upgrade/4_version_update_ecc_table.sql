6723095850b2cd751728c5de768ef0fb23570c02
LuK1337 <priv.luk@gmail.com>
2021-03-31 21:57:44 +0200

    sm8150-common: Extract from OnePlus7ProOxygen_21.P.35_OTA_0350_all_2103221240_0ebe9c8a78

---
+++ sm8150-common/proprietary/vendor/radio/qcril_database/upgrade/4_version_update_ecc_table.sql
@@ -0,0 +1,57 @@
+/*
+  Copyright (c) 2018 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+*/
+
+BEGIN TRANSACTION;
+
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 4);
+
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '840' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '854' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '855' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '856' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '857' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '858' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '859' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '860' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '861' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '862' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '863' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '864' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '865' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '866' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '867' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '868' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '869' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '870' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '871' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '872' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '873' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_mcc_mnc_table where MCC = '405' AND MNC = '874' AND NUMBER = '100';
+
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '840' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '854' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '855' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '856' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '857' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '858' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '859' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '860' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '861' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '862' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '863' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '864' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '865' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '866' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '867' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '868' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '869' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '870' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '871' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '872' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '873' AND NUMBER = '100';
+DELETE FROM qcril_emergency_source_voice_mcc_mnc_table where MCC = '405' AND MNC = '874' AND NUMBER = '100';
+
+COMMIT TRANSACTION;
