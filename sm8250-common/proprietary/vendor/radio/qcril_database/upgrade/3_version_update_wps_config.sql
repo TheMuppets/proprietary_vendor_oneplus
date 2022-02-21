e5b2de8438e4f1a4bba59be8c9371cbc7198af66
LuK1337 <priv.luk@gmail.com>
2022-02-21 22:18:51 +0100

    Import sm8250-common and its users from lineage-18.1 (ab4145f)
    Change-Id: I5ed31c1abe988113b0f47cbeb2d405e89720c64b

---
+++ sm8250-common/proprietary/vendor/radio/qcril_database/upgrade/3_version_update_wps_config.sql
@@ -0,0 +1,13 @@
+/*
+  Copyright (c) 2018 Qualcomm Technologies, Inc.
+  All Rights Reserved.
+  Confidential and Proprietary - Qualcomm Technologies, Inc.
+*/
+
+BEGIN TRANSACTION;
+
+INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 3);
+
+DELETE FROM qcril_operator_specific_config where MCC = '310' AND MNC = '410';
+
+COMMIT TRANSACTION;
