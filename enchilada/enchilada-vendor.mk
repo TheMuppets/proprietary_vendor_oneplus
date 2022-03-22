05e433ce97465ec22b8eda0c4e6b2fccfddb8f67
LuK1337 <priv.luk@gmail.com>
2022-03-22 21:55:14 +0100

    enchilada: Pull patched fingerprint init rc from stock
    Commented out 'writepid' should speed up auth a bit.
    
    Change-Id: Ida62664e725fc3e594155cd2db3a157bb3188df2

--- enchilada/enchilada-vendor.mk
+++ enchilada/enchilada-vendor.mk
@@ -17,6 +17,7 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/enchilada/proprietary/odm/etc/acdbdata/MTP/MTP_workspaceFile.qwsp:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MTP/MTP_workspaceFile.qwsp \
     vendor/oneplus/enchilada/proprietary/vendor/bin/hw/android.hardware.keymaster@3.0-service-qti:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.keymaster@3.0-service-qti \
     vendor/oneplus/enchilada/proprietary/vendor/etc/firmware/tp/fw_synaptics_17819.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/tp/fw_synaptics_17819.img \
+    vendor/oneplus/enchilada/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc \
     vendor/oneplus/enchilada/proprietary/vendor/etc/init/android.hardware.keymaster@3.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@3.0-service-qti.rc \
     vendor/oneplus/enchilada/proprietary/vendor/etc/sensors/config/sdm845_ak991x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sdm845_ak991x_0.json \
     vendor/oneplus/enchilada/proprietary/vendor/etc/sensors/config/sdm845_bmi160_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sdm845_bmi160_0.json \
