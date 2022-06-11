63456a65ee9f1f34b8b1d0e76be3967bfc0dd51f
Albert Tang <tangalbert919@yahoo.com>
2022-06-10 23:21:02 -0500

    dre: Extract from OnePlusN200Oxygen_10.O.11_OTA_011_all_2203021426_2dd4f42a2d0545a3
    Change-Id: I78a8298d4e69228ee5d54370aff751e6b47b764e

---
+++ dre/proprietary/vendor/etc/qcril_database/upgrade/config/7.0_config.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
+INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',7.0);
+INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES("persist.vendor.radio.shutdown_deactivate_timer", "90");
+UPDATE qcril_properties_table SET def_val="1" WHERE property="persist.vendor.radio.cs_srv_type";
+DELETE FROM qcril_properties_table WHERE property="persist.vendor.radio.hlos_prov_pref_auto";
